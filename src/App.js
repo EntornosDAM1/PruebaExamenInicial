2db5b45bad8c6c33e85dc2220d1732c59f73f3d7
EntornosDAM1 <hgomezv01@educantabria.es>
2022-02-13 20:18:27 +0100

    3 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './style.css';
-import Navbar from './components/BarraNavegacion';
+import BarraNavegacion from './components/BarraNavegacion';
 import Ejercicio1 from './components/Ejercicio1';
 import Ejercicio2 from './components/Ejercicio2';
 import Ejercicio3 from './components/Ejercicio3';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from 'react';
+import './style.css';
+
+//Importar componentes
+import {MenuElements} from './data/MenuElements';
+import React from 'react';
+import Header from './components/Header';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <>
-      <Navbar />
+    <Router>
+      <BarraNavegacion />
       <h1>Desarrollo de Interfaces: Examen React.js </h1>
       <p>
         Resuelve los ejercicios propuestos en los distintos apartados de la
@@ -17,9 +27,22 @@ export default function App() {
         tener el nombre examen-react-Nombre-Apellido1, donde nombre y apellido
         son los tuyos.
       </p>
+      {MenuElements.map((item) => {
+        return (
+          <Route
+            key={item.id}
+            path={item.path}
+            exact
+            component={item.component}
+          />
+        );
+      })}
+      {/*
       <Ejercicio1 />
       <Ejercicio2 />
       <Ejercicio3 />
+      */}
+      </Router>
     </>
   );
 }
--- src/components/BarraNavegacion.jsx
+++ src/components/BarraNavegacion.jsx
@@ -1,4 +1,9 @@
+import {MenuElements} from '../data/MenuElements'
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import {Container,Nav,NavDropdown,Form,FormControl,Button } from "react-bootstrap";
+import {NavBar, Nav,Offcanvas} from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class BarraNavegacion extends React.Component {
   constructor(props) {
@@ -7,19 +12,44 @@ class BarraNavegacion extends React.Component {
 
   render() {
     return (
-      <>
-        <ul>
-          <li>
-            <a href="#ej1">Ejercicio 1</a>
-          </li>
-          <li>
-            <a href="#ej2">Ejercicio 2</a>
-          </li>
-          <li>
-            <a href="#ej3">Ejercicio 3</a>
-          </li>
-        </ul>
-      </>
+      <Navbar bg="light" expand={false}>
+      <Container fluid>
+        <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+        <Navbar.Toggle aria-controls="offcanvasNavbar" />
+        <Navbar.Offcanvas
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+          placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              {/*}
+              <Nav.Link href="/Ejercicio1">Ejercicio1</Nav.Link>
+              <Nav.Link href="/Ejercicio2">Ejercicio 2</Nav.Link>
+              <Nav.Link href="/Ejercicio3">Ejercicio 3</Nav.Link>
+    */}
+            {MenuElements.map((item) => {
+            return (
+              <Nav.Link href={item.path}>{item.title}</Nav.Link>
+            );
+          })}
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
     );
   }
 }
